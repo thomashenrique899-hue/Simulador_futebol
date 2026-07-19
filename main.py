05d54b95a8717973df4373a377ab0aab84d1a36f
Thomas <thomashenrique899@gmail.com>
2026-07-19 17:17:16 -0300

    Aprimorado o sistema de cálculo de força do time e adicionada a tabela do campeonato

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     },
     "Flamengo": {
         "attack": 79,
@@ -21,7 +22,8 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     },
     "Palmeiras": {
         "attack": 78,
@@ -32,18 +34,8 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
-    },
-    "Corinthians": {
-        "attack": 78,
-        "midfield": 73,
-        "defense": 73,
-        "points": 0,
-        "wins": 0,
-        "draws": 0,
-        "losses": 0,
-        "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     },
     "Santos": {
         "attack": 74,
@@ -54,7 +46,8 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     },
     "Juventude": {
         "attack": 70,
@@ -65,7 +58,8 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     },
     "Ponte Preta": {
         "attack": 67,
@@ -76,13 +70,17 @@ teams = {
         "draws": 0,
         "losses": 0,
         "goals_for": 0,
-        "goals_against": 0
+        "goals_against": 0,
+        "played": 0
     }
 }
 
 #Calcula a força do time de acordo com os overs de defesa, meio e ataque
-def calculate_strength(team):
-    return team["attack"] + team["midfield"] + team["defense"] / 3
+def calculate_strength(team1, team2):
+    team_overall = (team1["attack"] + team1["midfield"] + team1["defense"]) / 3
+    attack_difference = team1["attack"] - team2["defense"]
+    
+    return team_overall + attack_difference
 
 #Calcula os gols de acordo com a força dos times
 def calculate_goals(strength):
@@ -96,13 +94,15 @@ def calculate_goals(strength):
 def simulate_match(home_team, away_team):
     if home_team in teams and away_team in teams:
 
-        home_team_strength = calculate_strength(teams[home_team])
-        away_team_strength = calculate_strength(teams[away_team])
+        home_team_strength = calculate_strength(teams[home_team], teams[away_team])
+        away_team_strength = calculate_strength(teams[away_team], teams[home_team])
 
         home_team_goals = calculate_goals(home_team_strength)
         away_team_goals = calculate_goals(away_team_strength)
 
-        #Calcula e insere no dicionário gols sofridos e feitos
+        #Calcula e insere no dicionário jogos, gols sofridos e feitos
+        teams[home_team]["played"] += 1
+        teams[away_team]["played"] += 1
         teams[home_team]["goals_for"] += home_team_goals
         teams[away_team]["goals_for"] += away_team_goals
         teams[home_team]["goals_against"] += away_team_goals
@@ -135,6 +135,39 @@ def simulate_match(home_team, away_team):
         for key in teams.keys():
             print(key)
 
-simulate_match("Palmeiras", "Cruzeiro")
-simulate_match("Juventude", "Flamengo")
-simulate_match("Ponte Preta", "Juventude")
+def show_table():
+
+    print(f"{'TEAM':<15}{'PTS':>4}{'P':>4}{'W':>4}{'D':>4}{'L':>4}{'GF':>5}{'GA':>5}{'GD':>5}")
+
+    organized = sorted(
+        teams.items(),
+        key=lambda item: (
+            item[1]["points"],
+            item[1]["wins"],
+            item[1]["goals_for"] - item[1]["goals_against"],
+            item[1]["goals_for"]
+        ),
+        reverse=True
+    )
+
+    for team, stats in organized:
+
+        goal_difference = stats["goals_for"] - stats["goals_against"]
+
+        print(
+            f"{team:<15}"
+            f"{stats['points']:>4}"
+            f"{stats['played']:>4}"
+            f"{stats['wins']:>4}"
+            f"{stats['draws']:>4}"
+            f"{stats['losses']:>4}"
+            f"{stats['goals_for']:>5}"
+            f"{stats['goals_against']:>5}"
+            f"{goal_difference:>5}"
+        )
+
+simulate_match("Cruzeiro", "Palmeiras")
+simulate_match("Santos", "Juventude")
+simulate_match("Flamengo", "Ponte Preta")
+
+show_table()
